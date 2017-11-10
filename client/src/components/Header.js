8a716399db702782be86590dea23948fd4997e97
Felipe Hernandez <felipeneo@hotmail.com>
2017-11-09 20:45:30 -0500

    create components directory

---
+++ client/src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+        <header className="App-header">
+          <div className="App">
+           <h1 className="App-title">Welcome to React</h1>
+          </div>
+        </header>
+    );
+  }
+}
+
+export default Header;
