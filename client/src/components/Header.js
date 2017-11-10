b7f7ab068ce3a0adf65e212ace51091345d1c9b3
Felipe Hernandez <felipeneo@hotmail.com>
2017-11-09 23:07:09 -0500

    Header.js add Navbar bootstarp components

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import '../App.css';
+
 
 class Header extends Component {
   render() {
     return (
-        <header className="App-header">
-          <div className="App">
-            <ul className="Nav">
-              <li>Home</li>
-              <li>Work</li>
-              <li>About</li>
-              <li>Contact</li>
-            </ul>
-          </div>
-        </header>
+        <Navbar inverse collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">FH digital development</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem eventKey={1} href="#">Link</NavItem>
+            <NavItem eventKey={2} href="#">Link</NavItem>
+          </Nav>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#">Link Right</NavItem>
+            <NavItem eventKey={2} href="#">Link Right</NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
