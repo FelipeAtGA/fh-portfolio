26393076a925483898b3317dee93db344a293c96
Felipe Hernandez <felipeneo@hotmail.com>
2017-11-09 21:01:25 -0500

    App.js add main tag

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,13 @@ class App extends Component {
     return (
       <div className='wraper'>
         <Header />
-        <div className="App">
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
+        <main>
+          <div className="App">
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+          </div>
+        </main>
       </div>
     );
   }
