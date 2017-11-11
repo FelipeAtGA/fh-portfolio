cf4e70e0bbf2ab3c1bae7b313805a0634bd13c06
Felipe Hernandez <felipeneo@hotmail.com>
2017-11-11 16:03:47 -0500

    Section.js delete width from img tag

--- client/src/components/Section.js
+++ client/src/components/Section.js
@@ -13,7 +13,7 @@ class Section extends Component {
             <p className="tagLine">Front End Web Developer</p>
           </div>
           <div id="logo">
-            <img className="img-responsive" src={logo} width="200px"/>
+            <img className="img-responsive" src={logo} />
           </div>
         </div>
       </div>
