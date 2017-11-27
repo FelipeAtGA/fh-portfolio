2410143c1923565bba450fe9d7aa6f589387b5fc
Felipe Hernandez <felipeneo@hotmail.com>
2017-11-26 21:45:35 -0500

    Header.js swap NavItem for li tags along with a tags. Also add i tags for font-awesome, and link to home from fh digital dev

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,19 +11,33 @@ class Header extends Component {
         <Navbar inverse collapseOnSelect>
           <Navbar.Header>
             <Navbar.Brand>
-              <a href="#">FH digital development</a>
+              <a href="../App.js">FH digital development</a>
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
           <Navbar.Collapse>
             <Nav>
-              <NavItem eventKey={1} href="#">Link</NavItem>
-              <NavItem eventKey={2} href="#">Link</NavItem>
+              <NavItem eventKey={1} href="#">Home</NavItem>
+              <NavItem eventKey={2} href="#">Work</NavItem>
+              <NavItem eventKey={3} href="#">About</NavItem>
             </Nav>
-            <Nav pullRight>
-              <NavItem eventKey={1} href="#">Link Right</NavItem>
-              <NavItem eventKey={2} href="#">Link Right</NavItem>
-            </Nav>
+            <ul className="nav navbar-nav navbar-right" >
+              <li>
+                <a href="https://github.com/FelipeAtGA" rel="noopener noreferrer" title="github" target="_blank">
+                  <i className="fa fa-github" aria-hidden="true"></i>
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/felipe-hernandez/" rel="noopener noreferrer" title="linked-in" target="_blank">
+                  <i className="fa fa-linkedin" aria-hidden="true"></i>
+                </a>
+              </li>
+              <li>
+                <a href="mailto:felipeneo@hotmail.com" rel="noopener noreferrer" title="email" target="_blank">
+                  <i className="fa fa-envelope-o" aria-hidden="true"></i>
+                </a>
+              </li>
+            </ul>
           </Navbar.Collapse>
         </Navbar>
         <Section />
