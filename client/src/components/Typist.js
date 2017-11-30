efb7666b3616875e305bb7baf04ccc9983bccdb2
Felipe Hernandez <felipeneo@hotmail.com>
2017-11-29 23:32:37 -0500

    Typist.js delete CSS classes

--- client/src/components/Typist.js
+++ client/src/components/Typist.js
@@ -3,9 +3,9 @@ import Typist from 'react-typist'; /* https://github.com/jstejada/react-typist *
 
 const Typewriter = () => {
   return (
-    <div className="typeWriter">
+    <div>
       <Typist>
-        <span className="welcomeTxt">Hello, and welcome to my site!<br />
+        <span>Hello, and welcome to my site!<br />
         In here, you will find work that I have done for web development, programming, and design.</span>
       </Typist>
     </div>
