9eecfacaec91953a87c8a3b50bf18253a7225743
Felipe Hernandez <felipeneo@hotmail.com>
2017-11-29 22:21:01 -0500

    App.js delete main tag and move it to Main component, also delete import App.css, Typest, glitchImagey

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
-import frontImage from './glitchImage.png';
-import Typist from 'react-typist'; /* https://github.com/jstejada/react-typist */
-import './App.css';
+import Main from './components/Main';
 
 class App extends Component {
   constructor(props){
@@ -16,17 +14,7 @@ class App extends Component {
     return (
       <div className='wraper'>
         <Header />
-        <main>
-          <div className="App">
-            <div className="typeWriter">
-              <Typist>
-                <span className="welcomeTxt">Hello, and welcome to my site!<br />
-                In here, you will find work that I have done for web development, programming, and design.</span>
-              </Typist>
-            </div>
-            <img className="img-responsive" src={frontImage} alt="Felipe Hernandez logo"/>
-          </div>
-        </main>
+        <Main />
       </div>
     );
   }
