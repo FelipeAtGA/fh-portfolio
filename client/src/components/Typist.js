c69c184372c0e600a3eb951dc476c8a1b5b06743
Felipe Hernandez <felipeneo@hotmail.com>
2017-11-29 23:57:19 -0500

    Typist.js delete div tag

--- client/src/components/Typist.js
+++ client/src/components/Typist.js
@@ -3,12 +3,10 @@ import Typist from 'react-typist'; /* https://github.com/jstejada/react-typist *
 
 const Typewriter = () => {
   return (
-    <div>
-      <Typist>
-        <span>Hello, and welcome to my site!<br />
-        In here, you will find work that I have done for web development, programming, and design.</span>
-      </Typist>
-    </div>
+    <Typist>
+      <span>Hello, and welcome to my site!<br />
+      In here, you will find work that I have done for web development, programming, and design.</span>
+    </Typist>
   );
 }
 
