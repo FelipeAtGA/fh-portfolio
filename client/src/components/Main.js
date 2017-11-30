68e2eed9680eff2c7232fd5a99f65ce71370774f
Felipe Hernandez <felipeneo@hotmail.com>
2017-11-29 23:31:42 -0500

    Main.js move typist tag to Typist component

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import frontImage from '../glitchImage.png';
-import Typist from 'react-typist'; /* https://github.com/jstejada/react-typist */
+import Typist from './Typist';
 import '../App.css';
 
 class Main extends Component {
@@ -8,12 +8,7 @@ class Main extends Component {
     return (
       <main>
         <div className="App">
-          <div className="typeWriter">
-            <Typist>
-              <span className="welcomeTxt">Hello, and welcome to my site!<br />
-              In here, you will find work that I have done for web development, programming, and design.</span>
-            </Typist>
-          </div>
+          <Typist />
           <img className="img-responsive" src={frontImage} alt="Felipe Hernandez logo"/>
         </div>
       </main>
