defd9f1ee9a843386b2d2a2c3acf1d0cb33e48ad
Felipe Hernandez <felipeneo@hotmail.com>
2017-12-05 11:33:53 -0500

    Welcomemessage.js created this component

---
+++ client/src/components/WelcomeMessage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Typist from './Typist';
+import './Welcomemessage.css';
+
+class Welcomemessage extends Component {
+  render() {
+    return (
+      <div className="messageWrapper">
+        <div className="brand">
+          <h1 className="name">Felipe Hernandez</h1>
+          <p className="ocupation">web developer</p>
+        </div>
+        <Typist />
+      </div>
+    );
+  }
+}
+
+export default Welcomemessage;
