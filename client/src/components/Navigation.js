d07d18f29125ea6f1da02e2b52a75f0b874eead8
Felipe Hernandez <felipeneo@hotmail.com>
2017-12-06 22:43:31 -0500

    create Navigation component

---
+++ client/src/components/Navigation.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Navbar.css';
+import logo from '../img/fh_logo_squared.png';
+
+const Navigation = () => {
+  return (
+    <nav>
+      <div className="logo_wrapper">
+        <a href="#">
+          <img src={logo} />
+        </a>
+      </div>
+      <ul>
+        <li><a href="#">Work</a></li>
+        <li><a href="#">About</a></li>
+        <li>
+          <a href="#">
+            <i class="fa fa-github" aria-hidden="true"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fa fa-linkedin" aria-hidden="true"></i>
+          </a>
+        </li>
+
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
