029096368a6a62ad6a4bb2cdc7466d634f9d0686
Felipe Hernandez <felipeneo@hotmail.com>
2017-12-26 16:15:04 -0500

    add some layout and structure with divs to webdevpro component

--- client/src/components/Webdevpro.js
+++ client/src/components/Webdevpro.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import "./Webdevpro.css"
+import Museumapp from "../img/museumVisitPlanner_homepage.png";
 
 class Webdevpro extends Component {
   constructor(props){
@@ -12,6 +14,14 @@ class Webdevpro extends Component {
     return (
       <div>
         <h1>Greetings from Webdevpro.js</h1>
+        <div className="webdevproGridWrapper clearfix">
+          <div className="imageWrapper">
+            <img src={ Museumapp } alt="Screenshot of the museum homepage app" />
+          </div>
+          <div>
+            <h3>Lorem Ipsun</h3>
+          </div>
+        </div>
       </div>
     );
   }
