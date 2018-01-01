fa2484c22d5635f016ca7a3ee76745faa5f1f5d0
Felipe Hernandez <felipeneo@hotmail.com>
2017-12-31 23:02:17 -0500

    add target blank

--- client/src/components/MuseumVisitApp.js
+++ client/src/components/MuseumVisitApp.js
@@ -11,7 +11,7 @@ const MuseumVisitApp = () => {
         <div className="workDescription">
           <h3>Museum Visit Planner</h3>
           <p>For my capstone project, I built a full CRUD application from the ground up. Using the MVC model, I scaffolded with node.js, then added back-end with express.js, and implemented react.js for the front-end. I also used a public API from a famous museum in nyc, using Json and axios. The project was completed in 14 days</p>
-          <a className="linkToLiveApp" href="https://dry-plains-91322.herokuapp.com/" >Live app</a>
+          <a className="linkToLiveApp" href="https://dry-plains-91322.herokuapp.com/" target="_black">Live app</a>
         </div>
         <div className="workTech">
           <h3>Technologies used:</h3>
