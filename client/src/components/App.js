ae3438dfd2883c2f8625e4f365a3aec53f2b9959
Felipe Hernandez <felipeneo@hotmail.com>
2018-01-07 14:17:04 -0500

    importe Modal component, add modal state, add and bind setModal method, add logic/functionality to setModal, pass setModal as props to Webdevpro component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router";
 import Header from "./Header";
+import Modal from "./Modal";
 import Webdevpro from "./Webdevpro";
 import Home from "./Home";
 
@@ -8,6 +9,23 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      modal: false,
+    }
+    this.setModal = this.setModal.bind(this);
+  }
+
+  setModal(event) {
+    let modal = document.getElementsByClassName("modalOverlay");
+    if (this.state.modal === false) {
+      modal[0].style.display="block";
+      this.setState({
+        modal: true,
+      })
+    }else{
+      modal[0].style.display="none";
+      this.setState({
+        modal: false,
+      })
     }
   }
 
@@ -19,7 +37,7 @@ class App extends Component {
         <main>
           <Switch>
             <Route exact path="/Webdevpro"
-              render={ () => <Webdevpro /> }
+              render={ () => <Webdevpro setModal={ this.setModal } /> }
             />
             <Route exact path="/" component={ Home }/>
           </Switch>
