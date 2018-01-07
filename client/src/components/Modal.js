e8a07b4279965b610339b332dd3ace90c7be4f87
Felipe Hernandez <felipeneo@hotmail.com>
2018-01-07 16:31:49 -0500

    add conditional functionality to Modal component for displaying appropriate image

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import unitCollectorGame from "../img/unitCollectorGame.jpg";
+import Museumapp from "../img/museumVisitPlanner_homepage.png";
 import "./Modal.css";
 
 class Modal extends Component {
@@ -9,13 +11,19 @@ class Modal extends Component {
   }
 
   render() {
+    let imageToRender = null;
+    if(this.props.imageUri === "unitCollectorImage"){
+      imageToRender = unitCollectorGame;
+    }else if (this.props.imageUri === "museumVisitPlanner"){
+      imageToRender = Museumapp;
+    }
     return (
       <div>
         <div className='modalOverlay' onClick = {() => {
               this.props.setModal();
             }}>
           <div className='modalContent'>
-          <img src={ this.props.imageUri } alt="Screenshot of the museum homepage app" />
+          <img src={ imageToRender } alt="Screenshot of the museum homepage app" />
           <div>
           <br />
           <p>( click anywhere to close this window )</p>
