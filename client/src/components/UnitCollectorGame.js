9a65ffd612f6ccbc7d83d65d1344656b6ced8dfe
Felipe Hernandez <felipeneo@hotmail.com>
2018-01-07 16:29:08 -0500

    add onClick functionality and pass image argument in MuseumVisitApp for Modal component

--- client/src/components/UnitCollectorGame.js
+++ client/src/components/UnitCollectorGame.js
@@ -1,32 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import unitCollectorGame from "../img/unitCollectorGame.jpg";
 
-const UnitCollectorApp = () => {
-  return (
-    <div>
-      <div className="webdevproGridWrapper clearfix">
-        <div className="imageWrapper">
-          <img src={ unitCollectorGame } alt="Screenshot of the museum javascript game app" />
-        </div>
-        <div className="workDescription">
-          <h3>Unit Collector: Js Game</h3>
-          <p>Unit Collector is going to be a version of the game Kaboom, a 1980s arcade game made by Activision for Atari. It has objects randomly falling from the top of the screen down, and the player has to catch these objects to avoid loosing the game. The player scores every time he/she catches an object. I was inspired to create Unit Collector from nostalgia for arcade games and the need to create something 2D.</p>
-          <a className="linkToLiveApp" href="http://unit-collector.bitballoon.com/" target="_black">Live app</a>
-        </div>
-        <div className="workTech">
-          <h3>Technologies used:</h3>
-          <ul>
-            <li className="listTech">HTML</li>
-            <li className="listTech">CSS -  animations</li>
-            <li className="listTech">JavaScript - objects</li>
-            <li className="listTech">JQuery</li>
-            <li className="listTech">Github</li>
-            <li className="listTech">CLI</li>
-          </ul>
+class UnitCollectorApp extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      imageName: "unitCollectorImage",
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="webdevproGridWrapper clearfix">
+          <div className="imageWrapper">
+            <img src={ unitCollectorGame } alt="Screenshot of the museum javascript game app"
+              onClick = { (image) => { this.props.setModal(this.state.imageName); } }
+            />
+          </div>
+          <div className="workDescription">
+            <h3>Unit Collector: Js Game</h3>
+            <p>Unit Collector is going to be a version of the game Kaboom, a 1980s arcade game made by Activision for Atari. It has objects randomly falling from the top of the screen down, and the player has to catch these objects to avoid loosing the game. The player scores every time he/she catches an object. I was inspired to create Unit Collector from nostalgia for arcade games and the need to create something 2D.</p>
+            <a className="linkToLiveApp" href="http://unit-collector.bitballoon.com/" target="_black">Live app</a>
+          </div>
+          <div className="workTech">
+            <h3>Technologies used:</h3>
+            <ul>
+              <li className="listTech">HTML</li>
+              <li className="listTech">CSS -  animations</li>
+              <li className="listTech">JavaScript - objects</li>
+              <li className="listTech">JQuery</li>
+              <li className="listTech">Github</li>
+              <li className="listTech">CLI</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default UnitCollectorApp;
