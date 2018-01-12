cc6ebe98d22f2f9354e9a51d02c7a1f339ec4ef1
Felipe Hernandez <felipeneo@hotmail.com>
2018-01-12 15:21:23 -0500

    add paragraph (work description) to Mywebpage component

--- client/src/components/Mywebpage.js
+++ client/src/components/Mywebpage.js
@@ -21,7 +21,7 @@ constructor(props){
           </div>
           <div className="workDescription">
             <h3>FH Portfolio website</h3>
-            <p>Lorem Ipsum</p>
+            <p>I created FH portfolio after finishing the Web Development Immersive Fellow (WDI) at General Assembly - New York City Tech Talent Pipeline. The goal was to revamp an old website I made during my college education at Hunter CUNY and to showcase the work that I’ve done in web development and emerging media. I chose React.js as the platform because of my passion for JavaScript and my love for the entertainment business and emerging media.</p>
             <a className="linkToLiveApp" href="http://localhost:3000/" target="_black">Live app</a>
             <a className="linkToAppInGithub" href="https://github.com/FelipeAtGA/fh-portfolio" target="_black">App on &nbsp;<i className="fa fa-github AppBttn" aria-hidden="true"></i></a>
           </div>
