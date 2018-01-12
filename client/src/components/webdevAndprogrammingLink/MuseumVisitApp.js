1b28dd1d52fda34ea6de1093b0eb2dcaa312ee53
Felipe Hernandez <felipeneo@hotmail.com>
2018-01-12 15:54:07 -0500

    create a folder for web dev + programming link to hold components on that link

---
+++ client/src/components/webdevAndprogrammingLink/MuseumVisitApp.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Museumapp from "../../img/museumVisitPlanner_homepage.png";
+
+class MuseumVisitApp extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      imageName: "museumVisitPlanner",
+    }
+  }
+
+
+  render() {
+    return (
+      <div>
+        <div className="webdevproGridWrapper clearfix cards">
+          <div className="imageWrapper">
+            <img src={ Museumapp } alt="Screenshot of the museum homepage app"
+                onClick = { (image) => { this.props.setModal(this.state.imageName); } }
+            />
+          </div>
+          <div className="workDescription">
+            <h3>Museum Visit Planner</h3>
+            <p>For my capstone project, I built a full CRUD application from the ground up. Using the MVC model, I scaffolded with node.js, then added back-end with express.js, and implemented react.js for the front-end. I also used a public API from a famous museum in nyc, using Json and axios. The project was completed in 14 days</p>
+            <a className="linkToLiveApp" href="https://dry-plains-91322.herokuapp.com/" target="_black">Live app</a>
+            <a className="linkToAppInGithub" href="https://github.com/FelipeAtGA/CH-Plan-A-Visit" target="_black">App on &nbsp;<i className="fa fa-github AppBttn" aria-hidden="true"></i></a>
+          </div>
+          <div className="workTech">
+            <h3>Technologies used:</h3>
+            <ul>
+              <li className="listTech">HTML + CSS</li>
+              <li className="listTech">RESTfull</li>
+              <li className="listTech">MVC model</li>
+              <li className="listTech">Node.js</li>
+              <li className="listTech">Express.js</li>
+              <li className="listTech">React.js</li>
+              <li className="listTech">Axios</li>
+              <li className="listTech">PostgreeSQL</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MuseumVisitApp;
