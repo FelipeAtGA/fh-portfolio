40e5a70495e969d73142475b6a1dee2b93de2f3a
Felipe Hernandez <felipeneo@hotmail.com>
2018-01-14 00:22:13 -0500

    fix eslintr errors

--- client/src/components/webdevAndprogrammingLink/Webdevpro.jsx
+++ client/src/components/webdevAndprogrammingLink/Webdevpro.jsx
@@ -1,25 +1,24 @@
-import React, { Component } from "react";
-import MuseumVisitApp from "./MuseumVisitApp";
-import UnitCollectorGame from "./UnitCollectorGame";
-import Mywebpage from "./Mywebpage";
-import "../../css/Webdevpro.css"
+import React, { Component } from 'react';
+import MuseumVisitApp from './MuseumVisitApp';
+import UnitCollectorGame from './UnitCollectorGame';
+import Mywebpage from './Mywebpage';
+import '../../css/Webdevpro.css';
 
 class Webdevpro extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-    }
+    };
   }
 
   render() {
-    console.log(this.state.type);
     return (
       <div>
         <hr />
         <h1 className="pageTitle">Web development<br />and programming</h1>
-        <MuseumVisitApp    setModal={ this.props.setModal } />
-        <UnitCollectorGame setModal={ this.props.setModal } />
-        <Mywebpage         setModal={ this.props.setModal } />
+        <MuseumVisitApp setModal={this.props.setModal} />
+        <UnitCollectorGame setModal={this.props.setModal} />
+        <Mywebpage setModal={this.props.setModal} />
       </div>
     );
   }
