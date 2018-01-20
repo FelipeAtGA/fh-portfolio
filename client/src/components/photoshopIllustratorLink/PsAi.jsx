a803c84a9ffbe88f2a67449bbdd653863fe1cef5
Felipe Hernandez <felipeneo@hotmail.com>
2018-01-20 16:00:59 -0500

    add html elements and content for third project, self portrait

--- client/src/components/photoshopIllustratorLink/PsAi.jsx
+++ client/src/components/photoshopIllustratorLink/PsAi.jsx
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import Iartposter from '../../img/posterD3-2.jpg';
 import Spaceroom from '../../img/Spaceroom.jpg';
+import Portrait from '../../img/selfPortrait.gif';
 import '../../css/PsAi.css';
 
 class PsAi extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      images: [Iartposter, Spaceroom],
-      titles: ['iARTmix poster', 'Defining space'],
+      images: [Iartposter, Spaceroom, Portrait],
+      titles: ['iARTmix poster', 'Defining space', 'Self portrait'],
       classes: 'projectWrapper col-xs-12 col-sm-12 col-md-3 col-lg-3',
       text: [
+        `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Nullam a ultrices nibh. Nullam non sapien in est rutrum pharetra.
+        stibulum id varius nisi. Donec eget viverra lorem, et mattis
+        lectus. Nulla viverra porta diam vel dapibus. Vestibulum placerat
+        ttis justo eget vehicula. Vestibulum quis massa orci. Fusce in
+        venenatis sapien.`,
         `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         Nullam a ultrices nibh. Nullam non sapien in est rutrum pharetra.
         stibulum id varius nisi. Donec eget viverra lorem, et mattis
@@ -42,6 +49,11 @@ class PsAi extends Component {
             <div className="projectimage" style={{ backgroundImage: `url(${this.state.images[1]})` }} />
             <h2>{this.state.titles[1]}</h2>
             <p>{this.state.text[1]}</p>
+          </div>
+          <div className={this.state.classes}>
+            <div className="projectimage" style={{ backgroundImage: `url(${this.state.images[2]})` }} />
+            <h2>{this.state.titles[2]}</h2>
+            <p>{this.state.text[2]}</p>
           </div>
         </div>
       </div>
