0af5766c869cf908bdc6dee7c2f4e7a811a557a1
Felipe Hernandez <felipeneo@hotmail.com>
2018-01-19 22:59:34 -0500

    import iARTmix image and add classNames

--- client/src/components/photoshopIllustratorLink/PsAi.jsx
+++ client/src/components/photoshopIllustratorLink/PsAi.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Iartposter from '../../img/posterD3-2.jpg';
+import '../../css/PsAi.css';
 
 class PsAi extends Component {
   constructor(props) {
@@ -12,8 +14,10 @@ class PsAi extends Component {
       <div>
         <hr />
         <h1 className="pageTitle">Photoshop and Illustrator</h1>
-        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-          <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+        <div className="wrapper col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <div className="poster col-xs-12 col-sm-12 col-md-4 col-lg-4">
+            <img src={Iartposter} alt="poster of the iARTmix exhibition" />
+            <h2>iARTmix poster</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             Nullam a ultrices nibh. Nullam non sapien in est rutrum pharetra.
             stibulum id varius nisi. Donec eget viverra lorem, et mattis
