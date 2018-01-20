c70d31a022a2770419d41751249ff573702b8f6e
Felipe Hernandez <felipeneo@hotmail.com>
2018-01-19 22:30:38 -0500

    add layout to PsAi component

--- client/src/components/photoshopIllustratorLink/PsAi.jsx
+++ client/src/components/photoshopIllustratorLink/PsAi.jsx
@@ -12,6 +12,17 @@ class PsAi extends Component {
       <div>
         <hr />
         <h1 className="pageTitle">Photoshop and Illustrator</h1>
+        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Nullam a ultrices nibh. Nullam non sapien in est rutrum pharetra.
+            stibulum id varius nisi. Donec eget viverra lorem, et mattis
+            lectus. Nulla viverra porta diam vel dapibus. Vestibulum placerat
+            ttis justo eget vehicula. Vestibulum quis massa orci. Fusce in
+            venenatis sapien.
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
