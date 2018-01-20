3ffffef4bb2208daa8eed31fdabd90489ccc86a2
Felipe Hernandez <felipeneo@hotmail.com>
2018-01-19 23:04:00 -0500

    change className

--- client/src/components/photoshopIllustratorLink/PsAi.jsx
+++ client/src/components/photoshopIllustratorLink/PsAi.jsx
@@ -14,7 +14,7 @@ class PsAi extends Component {
       <div>
         <hr />
         <h1 className="pageTitle">Photoshop and Illustrator</h1>
-        <div className="wrapper col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div className="PsAiwrapper col-xs-12 col-sm-12 col-md-12 col-lg-12">
           <div className="poster col-xs-12 col-sm-12 col-md-4 col-lg-4">
             <img src={Iartposter} alt="poster of the iARTmix exhibition" />
             <h2>iARTmix poster</h2>
