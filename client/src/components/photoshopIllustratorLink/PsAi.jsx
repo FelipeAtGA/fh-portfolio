1eaa0ea4de86d40ca7ae0f370b228396200ff36d
Felipe Hernandez <felipeneo@hotmail.com>
2018-01-19 21:44:44 -0500

    create directory for the photoshop and illustrator assets

---
+++ client/src/components/photoshopIllustratorLink/PsAi.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import '../../css/Webdevpro.css';
+
+class PsAi extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <hr />
+        <h1 className="pageTitle">Photoshop and Illustrator</h1>
+      </div>
+    );
+  }
+}
+
+export default PsAi;
