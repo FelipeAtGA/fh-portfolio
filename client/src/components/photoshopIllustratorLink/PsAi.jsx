abb8ce26a47711f2bf703d0257d037820d57267b
Felipe Hernandez <felipeneo@hotmail.com>
2018-01-20 16:20:47 -0500

    add state strings to pass as props to Modal and onClick function to setModal to all elements

--- client/src/components/photoshopIllustratorLink/PsAi.jsx
+++ client/src/components/photoshopIllustratorLink/PsAi.jsx
@@ -9,6 +9,7 @@ class PsAi extends Component {
     super(props);
     this.state = {
       images: [Iartposter, Spaceroom, Portrait],
+      imgpropsnames: ['iarposter', 'spaceroom', 'portrait'],
       titles: ['iARTmix poster', 'Defining space', 'Self portrait'],
       classes: 'projectWrapper col-xs-12 col-sm-12 col-md-3 col-lg-3',
       text: [
@@ -41,17 +42,38 @@ class PsAi extends Component {
         <h1 className="pageTitle">Photoshop and Illustrator</h1>
         <div className="PsAiwrapper col-xs-12 col-sm-12 col-md-12 col-lg-12">
           <div className={this.state.classes}>
-            <div className="projectimage" style={{ backgroundImage: `url(${this.state.images[0]})` }} />
+            <div
+              className="projectimage"
+              style={{ backgroundImage: `url(${this.state.images[0]})` }}
+              onClick={() => { this.props.setModal(this.state.imgpropsnames[0]); }}
+              onKeyDown={() => { this.props.setModal(); }}
+              role="button"
+              tabIndex="0"
+            />
             <h2>{this.state.titles[0]}</h2>
             <p>{this.state.text[0]}</p>
           </div>
           <div className={this.state.classes}>
-            <div className="projectimage" style={{ backgroundImage: `url(${this.state.images[1]})` }} />
+            <div
+              className="projectimage"
+              style={{ backgroundImage: `url(${this.state.images[1]})` }}
+              onClick={() => { this.props.setModal(this.state.imgpropsnames[1]); }}
+              onKeyDown={() => { this.props.setModal(); }}
+              role="button"
+              tabIndex="0"
+            />
             <h2>{this.state.titles[1]}</h2>
             <p>{this.state.text[1]}</p>
           </div>
           <div className={this.state.classes}>
-            <div className="projectimage" style={{ backgroundImage: `url(${this.state.images[2]})` }} />
+            <div
+              className="projectimage"
+              style={{ backgroundImage: `url(${this.state.images[2]})` }}
+              onClick={() => { this.props.setModal(this.state.imgpropsnames[2]); }}
+              onKeyDown={() => { this.props.setModal(); }}
+              role="button"
+              tabIndex="0"
+            />
             <h2>{this.state.titles[2]}</h2>
             <p>{this.state.text[2]}</p>
           </div>
