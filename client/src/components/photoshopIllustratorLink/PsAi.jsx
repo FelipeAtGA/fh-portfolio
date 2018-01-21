40d80de4cae4801333f495acbd1c83dd1aa1c11f
Felipe Hernandez <felipeneo@hotmail.com>
2018-01-20 21:40:06 -0500

    import new image for illustration work and add elements for it

--- client/src/components/photoshopIllustratorLink/PsAi.jsx
+++ client/src/components/photoshopIllustratorLink/PsAi.jsx
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 import Iartposter from '../../img/posterD3-2.jpg';
 import Spaceroom from '../../img/Spaceroom.jpg';
 import Portrait from '../../img/selfPortrait.gif';
+import Kiki from '../../img/kiki.jpg';
 import '../../css/PsAi.css';
 
 class PsAi extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      images: [Iartposter, Spaceroom, Portrait],
-      imgpropsnames: ['iarposter', 'spaceroom', 'portrait'],
-      titles: ['iARTmix poster', 'Defining space', 'Self portrait'],
-      classes: 'projectWrapper col-xs-12 col-sm-12 col-md-3 col-lg-3',
+      images: [Iartposter, Spaceroom, Portrait, Kiki],
+      imgpropsnames: ['iarposter', 'spaceroom', 'portrait', 'illustration'],
+      titles: ['iARTmix poster', 'Defining space', 'Self portrait', 'Illustration'],
+      classes: 'projectWrapper',
       text: [
+        `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Nullam a ultrices nibh. Nullam non sapien in est rutrum pharetra.
+        stibulum id varius nisi. Donec eget viverra lorem, et mattis
+        lectus. Nulla viverra porta diam vel dapibus. Vestibulum placerat
+        ttis justo eget vehicula. Vestibulum quis massa orci. Fusce in
+        venenatis sapien.`,
         `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         Nullam a ultrices nibh. Nullam non sapien in est rutrum pharetra.
         stibulum id varius nisi. Donec eget viverra lorem, et mattis
@@ -40,7 +47,7 @@ class PsAi extends Component {
       <div>
         <hr />
         <h1 className="pageTitle">Photoshop and Illustrator</h1>
-        <div className="PsAiwrapper col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div className="PsAiwrapper clearfix">
           <div className={this.state.classes}>
             <div
               className="projectimage"
@@ -76,6 +83,18 @@ class PsAi extends Component {
             />
             <h2>{this.state.titles[2]}</h2>
             <p>{this.state.text[2]}</p>
+          </div>
+          <div className={this.state.classes}>
+            <div
+              className="projectimage"
+              style={{ backgroundImage: `url(${this.state.images[3]})` }}
+              onClick={() => { this.props.setModal(this.state.imgpropsnames[3]); }}
+              onKeyDown={() => { this.props.setModal(); }}
+              role="button"
+              tabIndex="0"
+            />
+            <h2>{this.state.titles[3]}</h2>
+            <p>{this.state.text[3]}</p>
           </div>
         </div>
       </div>
