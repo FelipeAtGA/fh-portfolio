a9cb49f1b4ef54aedb84503514a75118b35d88d9
Felipe Hernandez <felipeneo@hotmail.com>
2018-01-23 16:25:52 -0500

    create videoAnimation directory. Also, create and edit VideoMation component file

---
+++ client/src/components/videoAnimation/VideoMation.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+
+class VidMation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <hr />
+        <h1 className="pageTitle">Photoshop and Illustrator</h1>
+        <div className="PsAiwrapper clearfix">
+          <video src="">Sorry, your browser doesn't support embeded videos</video>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default VidMation;
