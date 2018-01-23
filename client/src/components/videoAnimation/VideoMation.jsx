95fe53b046b6b038693dedace5636e4a63f2a745
Felipe Hernandez <felipeneo@hotmail.com>
2018-01-23 16:54:37 -0500

    import video theMeeting and add src to video tag

--- client/src/components/videoAnimation/VideoMation.jsx
+++ client/src/components/videoAnimation/VideoMation.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Themeeting from '../../video/theMeeting.mp4';
 import '../../css/VideoMation.css';
 
 class VideoMation extends Component {
@@ -14,7 +15,7 @@ class VideoMation extends Component {
         <hr />
         <h1 className="pageTitle">Digital Video and Animation</h1>
         <div className="videoWrapper clearfix">
-          <video src="">Sorry, your browser doesn't support embeded videos</video>
+          <video src={Themeeting}>Sorry, your browser doesn't support embeded videos</video>
         </div>
       </div>
     );
