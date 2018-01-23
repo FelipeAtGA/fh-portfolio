ac9bd0e6fd7beb3cda458c0499a47b9db182bf35
Felipe Hernandez <felipeneo@hotmail.com>
2018-01-23 15:53:04 -0500

    edit text for the description of each project in Photoshop and Illustrator

--- client/src/components/photoshopIllustratorLink/PsAi.jsx
+++ client/src/components/photoshopIllustratorLink/PsAi.jsx
@@ -14,30 +14,33 @@ class PsAi extends Component {
       titles: ['iARTmix poster', 'Defining space', 'Self portrait', 'Illustration'],
       classes: 'projectWrapper',
       text: [
-        `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Nullam a ultrices nibh. Nullam non sapien in est rutrum pharetra.
-        stibulum id varius nisi. Donec eget viverra lorem, et mattis
-        lectus. Nulla viverra porta diam vel dapibus. Vestibulum placerat
-        ttis justo eget vehicula. Vestibulum quis massa orci. Fusce in
-        venenatis sapien.`,
-        `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Nullam a ultrices nibh. Nullam non sapien in est rutrum pharetra.
-        stibulum id varius nisi. Donec eget viverra lorem, et mattis
-        lectus. Nulla viverra porta diam vel dapibus. Vestibulum placerat
-        ttis justo eget vehicula. Vestibulum quis massa orci. Fusce in
-        venenatis sapien.`,
-        `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Nullam a ultrices nibh. Nullam non sapien in est rutrum pharetra.
-        stibulum id varius nisi. Donec eget viverra lorem, et mattis
-        lectus. Nulla viverra porta diam vel dapibus. Vestibulum placerat
-        ttis justo eget vehicula. Vestibulum quis massa orci. Fusce in
-        venenatis sapien.`,
-        `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Nullam a ultrices nibh. Nullam non sapien in est rutrum pharetra.
-        stibulum id varius nisi. Donec eget viverra lorem, et mattis
-        lectus. Nulla viverra porta diam vel dapibus. Vestibulum placerat
-        ttis justo eget vehicula. Vestibulum quis massa orci. Fusce in
-        venenatis sapien.`,
+        `I used photographs from my own repertory and Photoshop CC to build the
+        design of the poster. The committee of the iARTmix exhibition commissioned
+        me with the task of creating a poster for the event. I developed three
+        different drafts with different designs and images, and the above poster
+        was the one selected. The iARTmix is an exhibition featuring students’ works
+        from courses at the crossroad of technology, music, and art.`,
+        `The goal of this project was to create an image that uses the tools and
+        editing capabilities of Photoshop and make a re-interpretation of the actual
+        space. So, for this project I manipulated a photography I took of a living
+        room. I included two external images to re—invent the space, one image of
+        my own stock and the other one from a free-licensing image stock site. I
+        aimed to give the image a totally new meaning that represents my creative
+        vision.`,
+        `For this project, my two subjects were a self-portrait and the use of
+        light as an element for composition. Additionally, I wanted to create
+        an image that uses the tools and editing capabilities of Photoshop while
+        also using the software layering functionality. So, I took pictures of
+        different elements that projected lights. Then, I edited them and selected
+        the ones I liked the most. And finally, I assembled all the different assets
+        in this composition.`,
+        `For my illustrator project, the goal was to create an image using all
+        the Illustrator tools and tricks that I knew possible at the time. So, I
+        used a photograph that I took during a vacation in Miami for its colorful
+        appearance and intricate design, placed it as a background, then traced
+        the image over a semi-transparent layer object. The final result was this
+        comic-kind-of-looking image that represents a real space and
+        event.`,
       ],
     };
   }
