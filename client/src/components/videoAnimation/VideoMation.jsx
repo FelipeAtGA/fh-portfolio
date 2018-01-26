7e9f8a85a1f67b2603122cd3382b8bba8befc589
Felipe Hernandez <felipeneo@hotmail.com>
2018-01-26 16:32:19 -0500

    add h4 tags and div class contentDescription to every project element, also add text, titles, and subtitles to every project element

--- client/src/components/videoAnimation/VideoMation.jsx
+++ client/src/components/videoAnimation/VideoMation.jsx
@@ -33,13 +33,16 @@ class VideoMation extends Component {
             />
             <h4>Stop motion animation:</h4>
             <h2>The Meeting</h2>
-            <p>This was my first animation project. The idea was to understand
-            animation mechanics, concepts, and technicalities. I built the scene
-            with random objects that I found around my house, then set it up and
-            started taking pictures of the characters’ different positions. I
-            used a Creative Commons song. The story is simple, two friends that
-            meet at a place to go have some coffee.
-            </p>
+            <div className="contentDescription">
+              <p>This was my first animation project. The idea was to understand
+              animation mechanics, concepts, and technicalities. I built the scene
+              with random objects that I found around my house, then set it up and
+              started taking pictures of the characters’ different positions. I
+              used a Creative Commons song. The story is simple, two friends that
+              meet at a place to go have some coffee.
+              </p>
+            </div>
+            <p className="downArrow">&#8675;</p>
           </div>
           <div className="projectWrapper">
             <div
@@ -50,8 +53,20 @@ class VideoMation extends Component {
               role="button"
               tabIndex="0"
             />
-            <h2>Video 2</h2>
-            <p>Paragraph 2</p>
+            <h4>Digital video production:</h4>
+            <h2>The Purple Box</h2>
+            <div className="contentDescription">
+              <p>Purple Box was a collaborative work. The title of the video and
+              inspiration came from a poem written by John Michael Wagner, who is
+              the narrator and collaborator in the production of this video. The
+              goal of the video is to match words/phrases with images to create a
+              narrative that portraits the poem. Hunter Stone and myself shot,
+              edited, and created the video. This video was featured during the
+              Netherlands Meets New York Screening at Hunter College, and at a
+              showcase for incoming students at Hunter College as well.
+              </p>
+            </div>
+            <p className="downArrow">&#8675;</p>
           </div>
           <div className="projectWrapper">
             <div
@@ -62,8 +77,23 @@ class VideoMation extends Component {
               role="button"
               tabIndex="0"
             />
-            <h2>Video 3</h2>
-            <p>Paragraph 3</p>
+            <h4>Character study animation:</h4>
+            <h2>The Little Prince</h2>
+            <div className="contentDescription">
+              <p>This project was made using Adobe After Effects and Illustrator.
+              The goal was to study a character’s movements and animate them. I
+              used the story of The Little Prince by Antoine de Saint-Exupéry
+              for inspiration. While it is not the literal portrayal of the story,
+                I included text excerpts from the book, with some adaptations of
+              my own, to create a small story. I also use my computers screen
+              reader to obtain a voice-over narrator. Then, I used a toy application
+              that modifies recordings to make it sound more robotic. I also
+              used music from Songafy, another toy application from the Apple
+              store. The project is a work in progress still, and I expect to
+              develop it further in the near future.
+              </p>
+            </div>
+            <p className="downArrow">&#8675;</p>
           </div>
           <div className="projectWrapper">
             <div
@@ -74,8 +104,22 @@ class VideoMation extends Component {
               role="button"
               tabIndex="0"
             />
-            <h2>Video 4</h2>
-            <p>Paragraph 4</p>
+            <h4>Typography animation:</h4>
+            <h2>Public Service Ann.</h2>
+            <div className="contentDescription">
+              <p>This project was made using Adobe After Effects and videos
+              shot with my cellphone. The goal was to manipulate and animate
+              typography while creating a public service announcement. My
+              subject was environmental protection. I got inspiration for
+              my subject from walking my dog everyday for 45 min, and my
+              concern for the problems that litter and plastic bags represent
+              for the environment and wild life. So, I narrowed my subject
+              to just plastic bags, and shot these plastic bags with my
+              cellphone while walking my dog. I also used music licensed
+              under Creative Commons. Credits are at the end of the video.
+              </p>
+            </div>
+            <p className="downArrow">&#8675;</p>
           </div>
           <div className="projectWrapper">
             <div
@@ -86,8 +130,23 @@ class VideoMation extends Component {
               role="button"
               tabIndex="0"
             />
-            <h2>Video 5</h2>
-            <p>Paragraph 5</p>
+            <h4>Defining space:</h4>
+            <h2>Defining Space</h2>
+            <div className="contentDescription">
+              <p>This was my first video ever made. So, the image is very
+              unsteady and the lightning is not as good. Even though the
+              quality of the video isn’t as professional as it should be,
+              I included it here because it speaks a lot about my ingenuity
+              and creative style. The subject was the space, and the goal
+              was to find and define space as alive entity. I walked through
+              my college campus and shot anything that I thought met the
+              criteria for the project. So, my personal goal was to bring
+              the space out of the background and convert it into a character
+              that narrates a story. I edit the movie in Final Cut on a Macintosh.
+              The music is by Bjork.
+              </p>
+            </div>
+            <p className="downArrow">&#8675;</p>
           </div>
           <div className="projectWrapper">
             <div
@@ -98,8 +157,18 @@ class VideoMation extends Component {
               role="button"
               tabIndex="0"
             />
-            <h2>Video 6</h2>
-            <p>Paragraph 6</p>
+            <h4>Processing.js sketch:</h4>
+            <h2>Untitled</h2>
+            <div className="contentDescription">
+              <p>I made this video using Processing.js. There is not story
+              or subject about it. The only goal for this project was to
+              explore the technology and create a program that reflected
+              the skills and concepts studies during our Game Programming
+              class. After we worked on Processing.js, we moved onto programming
+              with Unity for our final game programming project.
+              </p>
+            </div>
+            <p className="downArrow">&#8675;</p>
           </div>
         </div>
       </div>
