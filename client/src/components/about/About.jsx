30d5eae71152ae338ca2270d6bffb28a85253ce9
Felipe Hernandez <felipeneo@hotmail.com>
2018-01-29 15:56:04 -0500

    import resume img, switch ul for divs, and li for headings and paragraphs, add button to copy email and add onClick to props method, and anchor to download resume

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Aboutimage from '../../img/fhR.jpg';
+import Resume from '../../img/resume122117-v2a-img.jpg';
 import '../../css/About.css';
 
 class About extends Component {
@@ -20,16 +21,40 @@ class About extends Component {
             style={{ backgroundImage: `url(${Aboutimage})` }}
           />
           <div className="aboutTextWrapper">
-            <ul>
-              <li><h2>Felipe Hernandez - Web Developer</h2></li>
-              <li>I am passionate about problem solving and creating.
+            <div>
+              <h2>Felipe Hernandez - Web Developer</h2>
+              <p>I am passionate about problem solving and creating.
               So, I found programming to be the perfect tool for my
               personality and passion. I have a BA in Emerging Media
               Studies from Hunter College and a certificate from General
               Assembly. I am in a constant learning and creative mode.
               Please check out my resume in this page for more details.
-              </li>
-            </ul>
+              </p>
+            </div>
+            <div>
+              <h3>Contact</h3>
+              <div className="aboutContactDivs">
+                <input
+                  type="text"
+                  id="email"
+                  className="contactType"
+                  defaultValue="flphndz@yahoo.com"
+                  readOnly
+                />
+                <button
+                  className="contactButton"
+                  onClick={() => { this.props.clipboardCopy(); }}
+                  onKeyDown={() => { this.props.clipboardCopy(); }}
+                  tabIndex="0"
+                >
+                  ( click-copy to clipboard )
+                </button>
+              </div>
+              <div className="aboutContactDivs">
+                <p className="contactType">Resume</p>
+                <a href={Resume} download="Felipe Hernandez resume">click to download</a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
