85d0b148826d263569bacf917a650da0bb554003
Felipe Hernandez <felipeneo@hotmail.com>
2018-01-29 11:42:52 -0500

    import fhR image, import css file, edit h1 text, add image to div style, add ul element, and text to li

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Aboutimage from '../../img/fhR.jpg';
+import '../../css/About.css';
 
 class About extends Component {
   constructor(props) {
@@ -10,7 +12,26 @@ class About extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome form About me</h1>
+        <hr />
+        <h1 className="pageTitle">About</h1>
+        <div className="aboutWrapper clearfix">
+          <div
+            className="aboutImgtWrapper"
+            style={{ backgroundImage: `url(${Aboutimage})` }}
+          />
+          <div className="aboutTextWrapper">
+            <ul>
+              <li><h2>Felipe Hernandez - Web Developer</h2></li>
+              <li>I am passionate about problem solving and creating.
+              So, I found programming to be the perfect tool for my
+              personality and passion. I have a BA in Emerging Media
+              Studies from Hunter College and a certificate from General
+              Assembly. I am in a constant learning and creative mode.
+              Please check out my resume in this page for more details.
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
