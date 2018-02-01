59485aa5066f86aa3868e652b466aa240b389ff9
Felipe Hernandez <felipeneo@hotmail.com>
2018-02-01 14:39:50 -0500

    add form tag, change div acting as button for a button tag, add className to anchor tag for download, delete p tag for view online text

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -26,44 +26,50 @@ class About extends Component {
               <p>I am passionate about problem solving and creating.
               So, I found programming to be the perfect tool for my
               personality and passion. I have a BA in Emerging Media
-              Studies from Hunter College and a certificate from General
+              Studies from Hunter College and a Web Developer
+              certificate from General
               Assembly. I am in a constant learning and creative mode.
               Please check out my resume in this page for more details.
               </p>
             </div>
             <div>
-              <h3>Contact</h3>
               <div className="aboutContactDivs">
-                <input
-                  type="text"
-                  id="email"
-                  className="contactType"
-                  defaultValue="flphndz@yahoo.com"
-                  readOnly
-                />
+                <form>
+                  <i className="fa fa-envelope-o" />
+                  <input
+                    type="text"
+                    id="email"
+                    className="contactType"
+                    defaultValue="flphndz@yahoo.com"
+                    readOnly
+                  />
+                  <button
+                    className="contactButton"
+                    onClick={() => { this.props.clipboardCopy(); }}
+                    onKeyDown={() => { this.props.clipboardCopy(); }}
+                    tabIndex="0"
+                  >
+                     click-copy to clipboard
+                  </button>
+                </form>
+              </div>
+              <div className="aboutContactDivs">
+                <p className="contactTypeResume"><i className="fa fa-file-o" />Resume</p>
                 <button
                   className="contactButton"
-                  onClick={() => { this.props.clipboardCopy(); }}
-                  onKeyDown={() => { this.props.clipboardCopy(); }}
-                  tabIndex="0"
-                >
-                  ( click-copy to clipboard )
-                </button>
-              </div>
-              <div className="aboutContactDivs">
-                <p className="contactType">Resume</p>
-                <div
                   onClick={() => { this.props.setModal('resume'); }}
                   onKeyDown={() => { this.props.setModal(); }}
-                  role="button"
                   tabIndex="0"
                 >
-                  <p>view online</p>
-                </div>
-              </div>
-              <div className="aboutContactDivs">
-                <p className="contactType">Resume</p>
-                <a href={Resume} download="Felipe Hernandez resume">click to download</a>
+                  view online
+                </button>
+                <a
+                  className="contactAnchor"
+                  href={Resume}
+                  download="Felipe Hernandez resume"
+                >
+                  click to download
+                </a>
               </div>
             </div>
           </div>
