0d85398d620fd695376b4a93ae6b2b4a8c1407d0
Felipe Hernandez <felipeneo@hotmail.com>
2018-07-16 17:25:45 -0400

    edit path to new resume image on Modal and About components, and edit word in text in About

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Aboutimage from '../../img/fhR.jpg';
-import Resume from '../../img/resume122117-v2d-5.jpg';
+import Resume from '../../img/resume070218-v2.jpg';
 import '../../css/About.css';
 
 class About extends Component {
@@ -23,7 +23,7 @@ class About extends Component {
           <div className="aboutTextWrapper">
             <div>
               <h2>Felipe Hernandez - Web Developer</h2>
-              <p>I am passionate about problem solving and creating.
+              <p>I am passionate about problem solving and creativity.
               So, I found programming to be the perfect tool for my
               personality and passion. I have a BA in Emerging Media
               Studies from Hunter College and a Web Developer
